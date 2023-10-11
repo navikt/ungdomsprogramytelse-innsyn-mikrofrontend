927a17e82e190901c555a65837ed6040dcc9514f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-10-11 15:12:19 +0200

    Lager komponent etter "Produktkort" stilen til tms-oversikt.

--- src/components/Komponent.tsx
+++ src/components/Komponent.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { LinkPanel } from "@navikt/ds-react";
-import "./Komponent.css";
-import { PersonTallShortIcon } from "@navikt/aksel-icons";
+import { BodyLong, Heading } from "@navikt/ds-react";
+import { ChevronRightIcon, PersonTallShortIcon } from "@navikt/aksel-icons";
 import { innsynDinePleiepengerUrl } from "../api/urls";
+import styles from "./Komponent.module.css";
 
 interface Props {
   tittel: string;
@@ -11,21 +11,20 @@ interface Props {
 
 const Komponent = ({ tittel, undertittel }: Props) => {
   return (
-    <div className="komponent">
-      <LinkPanel border href={innsynDinePleiepengerUrl}>
-        <LinkPanel.Title>
-          <div className="flex items-center space-x-4">
-            <div className="text-6xl flex-shrink-0 grid place-content-center">
-              <PersonTallShortIcon title="a11y-title" fontSize="3rem" />
-            </div>
-            <div>
-              <LinkPanel.Title id="AboutExpansionCardTitle">{tittel}</LinkPanel.Title>
-            </div>
-          </div>
-        </LinkPanel.Title>
-        <LinkPanel.Description>{undertittel}</LinkPanel.Description>
-      </LinkPanel>
-    </div>
+    <a className={styles.container} href={innsynDinePleiepengerUrl}>
+      <div className={styles.ikonOgTekstContainer}>
+        <div aria-hidden>
+          <PersonTallShortIcon title="a11y-title" fontSize="3rem" />
+        </div>
+        <div>
+          <Heading size="small" level="2">
+            {tittel}
+          </Heading>
+          <BodyLong size="medium">{undertittel}</BodyLong>
+        </div>
+      </div>
+      <ChevronRightIcon className={styles.chevron} aria-hidden fontSize="24px" />
+    </a>
   );
 };
 
